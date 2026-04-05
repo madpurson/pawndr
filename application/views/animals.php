fdaf0ad16b6f5ea170ee411b6cb5830209769163
Miki <110220534+madpurson@users.noreply.github.com>
2026-04-05 17:42:04 +0800

    adjusted new data properties to be shown in general [animals] route

--- application/views/animals.php
+++ application/views/animals.php
@@ -4,10 +4,33 @@
     </head>
     <body>
         <h1>Animals</h1>
+        
+        <?php foreach ($animals as $animal): ?>
         <ul>
-            <?php foreach ($animals as $animal): ?>
-                <li><?php echo $animal['name']; ?></li>
-            <?php endforeach; ?>
+            <li>
+                <a href="<?php echo base_url('animals/view/' . $animal['id']); ?>">
+                    <img src="<?php echo $animal['image_url']; ?>" alt="<?php echo $animal['name']; ?>" width="100">
+                </a>
+                <br>
+                <?php echo $animal['name']; ?>
+                <br>
+                <span> <?php echo $animal['type']; ?> </span>
+            </li>
+            <li>
+                <?php echo $animal['description']; ?>
+            </li>
+            <li>
+                Breed: <?php echo $animal['breed']; ?>
+            </li>
+            <li>
+                Age: <?php echo $animal['age']; ?>
+            </li>
+            <li>
+                Sex: <?php echo $animal['sex']; ?>
+            </li>
+            <hr>
         </ul>
+        <?php endforeach; ?>
+        
     </body>
 </html>
